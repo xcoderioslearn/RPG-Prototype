0abcb434981f85c72e9594b761481036218e0bb8
xcoderioslearn <tradesignalshustling@gmail.com>
2025-10-16 14:44:23 +0100

    4.4.1 Retest
    4.4.1 Max Grid Map

--- scenes/enemies/bear.gd
+++ scenes/enemies/bear.gd
@@ -3,9 +3,12 @@ extends CharacterBody3D
 @onready var animation_player: AnimationPlayer = $Sketchfab_Scene/AnimationPlayer
 
 @export var speed: float = 2.0
-
+var xmovement = 1
 func _ready() -> void:
 	animation_player.play("Armature|A Wait")
 	
 func _physics_process(delta: float) -> void:
+	velocity.x = xmovement
+	velocity.y = 0
+	velocity.y = 0
 	move_and_slide()
